537ddd3c8e33891c6a1d4c70f4e24376fa9e5b68
kavindu wimalasooriya <agkmwimalasooriya@gmail.com>
2021-09-09 12:50:50 +0530

    Add IplatformRepo and PlatformRepo

--- DataAccess/IplatformRepo.cs
+++ DataAccess/IplatformRepo.cs
@@ -1,7 +1,14 @@
+using System.Collections.Generic;
+using PlateformService.Models;
+
 namespace PlateformService.DataAccess
 {
     public interface IPlatformRepo
     {
+        bool SaveChanges();
+        IEnumerable<Platform> GetAllPlatforms();
+        Platform GetPlatformById(int id);
+        void CreatePlatform(Platform platform);
         
     }
 }
